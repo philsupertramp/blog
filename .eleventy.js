b5902d96a8caa7553e5fbff24b4e12b202abe92d
Philipp Zettl <zettl.philipp@gmail.com>
2024-03-24 12:02:21 +0100

    replace inline mathjax with plugin and fix some small things

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const mathjaxPlugin = require("eleventy-plugin-mathjax");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/_includes/assets/*");
 
+    eleventyConfig.addPlugin(mathjaxPlugin);
     return {
         dir: {
             includes: "_includes",
